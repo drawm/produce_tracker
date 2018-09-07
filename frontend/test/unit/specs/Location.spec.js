488cd1ca5a9dfd577908b242177373564ea5faa2
mmukandila <mmukandila@outbox.com>
2018-09-07 13:58:51 -0400

    Adding more to Locations

--- frontend/test/unit/specs/Location.spec.js
+++ frontend/test/unit/specs/Location.spec.js
@@ -15,4 +15,44 @@ describe('Location.vue', () => {
   it('should render correct contents', () => {
     expect(wrp.find('div.v-toolbar__title').text()).toEqual('Locations')
   })
+
+  it('does not display element if there are no items', () => {
+    wrp.setData({ locations: [] })
+    expect(wrp.find('ul.v-expansion-panel').isEmpty()).toBe(true)
+  })
+
+  it('displays element when there are items', () => {
+    wrp.setData({ 
+      locations: [
+        {
+          name: 'Super Market One',
+          streetNumber: 1234,
+          streetName: 'The Street',
+          city: 'Montreal',
+          province: 'Quebec',
+          country: 'Canada',
+          postalcode: 'H0H0H0'
+        },
+        {
+          name: 'Super Market One',
+          streetNumber: 1234,
+          streetName: 'The Street',
+          city: 'Montreal',
+          province: 'Quebec',
+          country: 'Canada',
+          postalcode: 'H0H0H0'
+        },
+        {
+          name: 'Super Market One',
+          streetNumber: 1234,
+          streetName: 'The Street',
+          city: 'Montreal',
+          province: 'Quebec',
+          country: 'Canada',
+          postalcode: 'H0H0H0'
+        }
+      ] 
+    })
+    expect(wrp.findAll('li.v-expansion-panel__container').length).toBe(3)
+  })
 })
